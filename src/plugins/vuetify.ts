6347b951b38fefe094f76f80c40fa98ea95a2aea
Markus Schnider <mschnide@gmail.com>
2022-11-09 14:41:01 +0100

    default project

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -10,10 +10,28 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { de, en, fr, it } from 'vuetify/locale'
+
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  blueprint: md3,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+  locale: {
+    locale: 'de',
+    fallback: 'en',
+    messages: { de, en, fr, it }
+  },
   theme: {
+    defaultTheme: 'dark',
     themes: {
       light: {
         colors: {
